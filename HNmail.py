6800d0a4c93f33795bcf25cd0b0ec2dc15465eaa
Max Foccroulle <foccroullemax@gmail.com>
2024-02-07 15:21:12 +0100

    Ajout de la fonction d'envoi d'email

---
+++ HNmail.py
@@ -0,0 +1,16 @@
+import emailsender
+import webscraping
+
+
+def main():
+    url = 'https://news.ycombinator.com/news'
+    soup = webscraping.get_soup_from_url(url)
+    links = webscraping.extract_links(soup, 100)
+    email_content = emailsender.create_email_content(
+        'email_template.html', links)
+    emailsender.send_email(emailsender.read_personaldata_file(
+        'personaldata.txt'), "News de Hacker News", email_content)
+
+
+if __name__ == '__main__':
+    main()
